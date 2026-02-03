ca1765a8c9fd48cd7a48ed0f63d8bc32c125b40c
febe <febe.verelst@student.howest.be>
2026-02-03 20:19:24 +0100

    astro config basepath fix

--- cookbook/astro.config.mjs
+++ cookbook/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: "https://febeverelst.github.io",
-    base: "/cookbook",
+    base: "/algorave_FE-VER/cookbook",
 });
